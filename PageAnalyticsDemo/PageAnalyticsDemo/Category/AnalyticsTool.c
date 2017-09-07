71fae979c5ad0f05580074214635bb111729ede5
shuima <fenggy101@126.com>
2017-09-07 23:09:49 +0800

    modify comment

--- PageAnalyticsDemo/PageAnalyticsDemo/Category/AnalyticsTool.c
+++ PageAnalyticsDemo/PageAnalyticsDemo/Category/AnalyticsTool.c
@@ -16,7 +16,7 @@ void analytics_swizzleMethod(Class cls, SEL originSel, SEL swizzledSel)
     IMP swizzledImp = method_getImplementation(swizzledMethod);
     const char *types = method_getTypeEncoding(swizzledMethod);
     BOOL success = class_addMethod(cls, originSel, swizzledImp, types);
-    // 如果子类没有实现, 可以避免替换父类的方法, 而对父类的其他子类产生影响(如果你认为判断没有必要, 可以注释掉if判断, 只留下else里的语句, 我给你留了惊喜, O(∩_∩)O)
+    // 如果子类没有实现, 可以避免替换父类的方法, 而对父类的其他子类产生影响(如果你认为判断没有必要, 可以注释掉if判断, 只留下else里的语句试一下, 有一个崩溃)AAAWWWAWW
     if (success) {
         IMP originImp = method_getImplementation(originMethod);
         const char *types = method_getTypeEncoding(originMethod);
